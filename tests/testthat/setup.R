10378285ad4b1ad8dd886e362994964aa0c06a60
agenius-logesh <logesh@aggregate-genius.com>
2021-07-06 00:53:56 -0700

    refactored

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -4,5 +4,6 @@ require(limma)
 require(DGEobj)
 require(DGEobj.plots)
 require(DGEobj.utils)
+library(ggplot2)
 
-t_obj1 <- readRDS(system.file("exampleObj.RDS", package = "DGEobj", mustWork = TRUE))
+t_obj <- readRDS("/efs/bms_shared/data/cdb/APJ_RHR_RatHeart_29Nov2016.RDS")
